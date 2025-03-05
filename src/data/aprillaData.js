9ce32cd2930e8b99a38115283881cd06213a079f
Marc Jersey Castro <marcjerseycastro@gmail.com>
2025-03-05 10:53:34 +0800

    aprilla done ( ;

---
+++ src/data/aprillaData.js
@@ -0,0 +1,44 @@
+import aprilla1 from '/aprilla.jpg'
+import aprilla2 from '/aprilla2.jpeg'
+import aprilla3 from '/aprilla3.jpg'
+import aprilla4 from '/aprilla4.jpg'
+
+
+export const aprillaData = [
+    {
+        name: "Aprilia RS 150",
+        engine: "150cc single-cylinder",
+        power: "~18 HP",
+        topSpeed: "~130 km/h",
+        features: "Lightweight chassis, aggressive styling",
+        image: aprilla1,
+        category: "155cc"
+    },
+    {
+        name: "Aprilia RS 457",
+        engine: "457cc parallel-twin",
+        power: "~48 HP",
+        topSpeed: "~190 km/h",
+        features: "Dual-channel ABS, ride-by-wire, sporty aerodynamics",
+        image: aprilla2,
+        category: "300cc"
+    },
+    {
+        name: "Aprilia Tuono 660",
+        engine: "659cc parallel-twin",
+        power: "~100 HP",
+        topSpeed: "~225 km/h",
+        features: "Quickshifter, electronic rider aids, semi-faired design",
+        image: aprilla3,
+        category: "600cc"
+    },
+    {
+        name: "Aprilia RSV4 1100",
+        engine: "1099cc V4",
+        power: "~217 HP",
+        topSpeed: "~300+ km/h",
+        features: "Advanced aerodynamics, MotoGP tech, full electronics package",
+        image: aprilla4,
+        category: "1000cc"
+    }
+];
--- src/pages/Aprillia/ApriliaRSV41100.jsx
+++ src/pages/Aprillia/ApriliaRSV41100.jsx
@@ -1,13 +1,47 @@
 import React from 'react'
 import AprilliaModelNavigation from '../../components/AprilliaModelNavigation'
 
+import { aprillaData } from '../../data/aprillaData'
+
 export const ApriliaRSV41100 = () =>
 {
+
+    const aprilla600 = aprillaData.filter(bike => bike.category == "1000cc");
+
     return (
-        <div>
+        <div className='flex flex-row items-center justify-around'>
 
             <AprilliaModelNavigation />
-            ApriliaRSV41100
+
+
+            <section className='text-center flex flex-col items-center justify-center'>
+                {aprilla600.map((bike, index) => (
+
+
+                    <div key={index} className='flex flex-col items-center justify-center'>
+                        <img className='h-[50vh]' src={bike.image} alt="Aprilla" />
+                        <h1 className=' my-4 text-3xl font-bold'>{bike.name}</h1>
+                        <table className='border p-2  '>
+                            <thead>
+                                <tr>
+                                    <th className='border p-2'>Engine</th>
+                                    <th className='border p-2'>Power</th>
+                                    <th className='border p-2'>Top Speed</th>
+                                    <th className='border p-2'>Features</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td className='border p-2'>{bike.engine}</td>
+                                    <td className='border p-2'>{bike.power}</td>
+                                    <td className='border p-2'>{bike.topSpeed}</td>
+                                    <td className='border p-2'>{bike.features}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                ))}
+            </section>
         </div>
     )
 }
